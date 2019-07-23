862fb147864a9abcab8e90c08bb7dcb2f71dd59d
Felipe Fiebig <felipe.fiebig@continuum.cl>
2019-07-23 17:03:05 -0400

    Try webdriverio on Travis

---
+++ tests2/specs/integration/test_spec.js
@@ -0,0 +1,29 @@
+const assert = require('assert');
+
+describe('Using Webpay Plus', () => {
+    it('should let you pay with credit card', () => {
+        browser.url('http://localhost:9000');
+        $('a=Webpay Plus Normal').click();
+        $('input[value="Continuar »"]').click();
+        // Change this, it's awful, just leave it to test travis
+        $('h1=Selecciona tu medio de pago').waitForExist();
+        $('h3=Tarjeta de crédito').click();
+        $('input[name="cardNumber"]').setValue("4051 8856 0044 6623");
+        $('input[name="cvv"]').setValue("123");
+        $('button.button.new-marg.next-padd').click();
+        $('button.button.new-marg.next-padd').click();
+        browser.pause(6000);
+        $('frameset>frame[name="transicion"]').waitForExist();
+        browser.frame($('frameset>frame[name="transicion"]').value);
+        $('input#rutClient').waitForExist();
+        $('input#rutClient').setValue('11.111.111-1');
+        $('input#passwordClient').setValue('123');
+        $('input[type=submit]').click();
+        browser.pause(3000);
+        $('input[type=submit]').click();
+        $('samp=Pago ACEPTADO por webpay (se deben guardatos para mostrar voucher)').waitForExist();
+        $('input[value="Continuar »"]').click();
+        $('a=Ir a detalle de la compra').click();
+        $('samp=Transacion finalizada')
+    });
+});
